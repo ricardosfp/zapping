7289d7a55a11c6b6454cbbf02b49e6a6524d878e
Ricardo Pinto <ricardofelgueiras1993@gmail.com>
2024-11-08 15:41:32 +0100

    added tests for the ZappingRepositoryImpl (#16)

--- app/src/test/java/com/ricardosfp/zapping/domain/match/MatchParserImplTest.kt
+++ app/src/test/java/com/ricardosfp/zapping/domain/match/MatchParserImplTest.kt
@@ -11,25 +11,33 @@ class MatchParserImplTest {
 
     private lateinit var matchParser: MatchParser
 
-    //    "Peñarol x Atlético Mineiro - 14/05 23:00 - SportTv1"
-    private val validArticleDateString = "Tue, 14 May 2024 23:00:00"
-    private val validArticleHomeTeam = "Peñarol"
-    private val validArticleAwayTeam = "Atlético Mineiro"
-    private val validArticleChannel = "SportTv1"
-    private val validArticleTitle = String.format("%s x %s - 14/05 23:00 - %s",
-        validArticleHomeTeam,
-        validArticleAwayTeam,
-        validArticleChannel)
-    private val validArticle = MyArticle(title = validArticleTitle, date = validArticleDateString)
+    companion object {
+        //    "Peñarol x Atlético Mineiro - 14/05 23:00 - SportTv1"
+        private const val VALID_ARTICLE_DATE_STRING = "Tue, 14 May 2024 23:00:00"
+        private const val VALID_ARTICLE_HOME_TEAM = "Peñarol"
+        private const val VALID_ARTICLE_AWAY_TEAM = "Atlético Mineiro"
+        private const val VALID_ARTICLE_CHANNEL = "SportTv1"
+        private val validArticleTitle = String.format(
+            "%s x %s - 14/05 23:00 - %s",
+            VALID_ARTICLE_HOME_TEAM,
+            VALID_ARTICLE_AWAY_TEAM,
+            VALID_ARTICLE_CHANNEL)
+        private val validArticle = MyArticle(
+            title = validArticleTitle, date = VALID_ARTICLE_DATE_STRING)
 
-    // invalid articles
-    private val invalidArticleInvalidDate = MyArticle(title = validArticleTitle, date = "14 May 24")
-    private val invalidArticleInvalidHomeTeam = MyArticle(title = " x " + "Atlético Mineiro - 14/05 23:00 - SportTv1",
-        date = validArticleDateString)
-    private val invalidArticleInvalidAwayTeam = MyArticle(title = "Peñarol x  - 14/05 23:00 - SportTv1",
-        date = validArticleDateString)
-    private val invalidArticleInvalidChannel = MyArticle(title = "Peñarol x " + "Atlético Mineiro - 14/05 23:00 - ",
-        date = validArticleDateString)
+        // invalid articles
+        private val invalidArticleInvalidDate = MyArticle(
+            title = validArticleTitle, date = "14 May 24")
+        private val invalidArticleInvalidHomeTeam = MyArticle(
+            title = " x " + "Atlético Mineiro - 14/05 23:00 - SportTv1",
+            date = VALID_ARTICLE_DATE_STRING)
+        private val invalidArticleInvalidAwayTeam = MyArticle(
+            title = "Peñarol x  - 14/05 23:00 - SportTv1", date = VALID_ARTICLE_DATE_STRING)
+        private val invalidArticleInvalidChannel = MyArticle(
+            title = "Peñarol x " + "Atlético Mineiro - 14/05 23:00 - ",
+            date = VALID_ARTICLE_DATE_STRING)
+    }
+
 
     @BeforeEach
     fun setupInstance() {
@@ -46,10 +54,10 @@ class MatchParserImplTest {
         val match = (matchParseResult as MatchParseSuccess).match
 
         assertEquals(validArticleTitle, match.originalText)
-        assertEquals(validArticleHomeTeam, match.homeTeam)
-        assertEquals(validArticleAwayTeam, match.awayTeam)
+        assertEquals(VALID_ARTICLE_HOME_TEAM, match.homeTeam)
+        assertEquals(VALID_ARTICLE_AWAY_TEAM, match.awayTeam)
         assertEquals(Date(2024 - 1900, 4, 14, 23, 0, 0), match.date)
-        assertEquals(validArticleChannel, match.channel)
+        assertEquals(VALID_ARTICLE_CHANNEL, match.channel)
         assertEquals(validArticleTitle, match.originalText)
     }
 
