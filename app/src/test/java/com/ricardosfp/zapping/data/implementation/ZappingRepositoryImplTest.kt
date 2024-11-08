9e2fc8280e9b7f2d8db7752ec1b5fee4031098ec
ricardo <ricardofelgueiras1993@gmail.com>
2024-11-08 15:38:15 +0100

    added tests for the ZappingRepositoryImpl

--- app/src/test/java/com/ricardosfp/zapping/data/implementation/ZappingRepositoryImplTest.kt
+++ app/src/test/java/com/ricardosfp/zapping/data/implementation/ZappingRepositoryImplTest.kt
@@ -7,11 +7,36 @@ import io.mockk.*
 import kotlinx.coroutines.test.*
 import okhttp3.internal.*
 import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
 
 class ZappingRepositoryImplTest {
 
     private lateinit var zappingRepository: ZappingRepository
     private lateinit var myRssParser: MyRssParser
+
+    companion object {
+        private val parserOutput = immutableListOf(
+            MyRssItem(
+                title = "Al Hilal x Al-Ettifaq - 08/11 14:45 - SportTV 1",
+                pubDate = "Fri, 08 Nov 2024 14:45:00"),
+            MyRssItem(
+                title = "Al-Riyadh x Al Nassr - " + "08/11 17:00 - SportTV 1",
+                pubDate = "Fri, 08 Nov 2024 17:00:00"),
+            MyRssItem(
+                title = "FC Vizela x GD Chaves - 08/11 18:00 - SportTV +",
+                pubDate = "Fri, 08 Nov 2024 18:00:00"))
+
+        private val expectedRepositoryOutput = immutableListOf(
+            MyArticle(
+                title = "Al Hilal x Al-Ettifaq - 08/11 14:45 - SportTV 1",
+                date = "Fri, 08 Nov 2024 14:45:00"),
+            MyArticle(
+                title = "Al-Riyadh x Al Nassr - " + "08/11 17:00 - SportTV 1",
+                date = "Fri, 08 Nov 2024 17:00:00"),
+            MyArticle(
+                title = "FC Vizela x GD Chaves - 08/11 18:00 - SportTV +",
+                date = "Fri, 08 Nov 2024 18:00:00"))
+    }
 
     @BeforeEach
     fun setupInstance() {
@@ -19,15 +44,24 @@ class ZappingRepositoryImplTest {
         zappingRepository = ZappingRepositoryImpl(myRssParser)
     }
 
-
-    // todo maybe check if the conversion to [com.ricardosfp.zapping.data.repository.model
-    //  .MyArticle] is correct
     @Test
     fun `good parsing returns GetArticlesSuccess`() = runTest {
 
-        coEvery { myRssParser.parse(any()) } returns RssParseSuccess(immutableListOf())
+        coEvery { myRssParser.parse(any()) } returns RssParseSuccess(parserOutput)
 
         val result = zappingRepository.getArticles("")
-        Assertions.assertEquals(GetArticlesSuccess::class, result::class)
+        assertEquals(GetArticlesSuccess::class, result::class)
+        if (result is GetArticlesSuccess) {
+            assertIterableEquals(expectedRepositoryOutput, result.articles)
+        }
+    }
+
+    @Test
+    fun `bad parsing returns GetArticlesError`() = runTest {
+
+        coEvery { myRssParser.parse(any()) } returns RssParseException(Exception())
+
+        val result = zappingRepository.getArticles("")
+        assertEquals(GetArticlesError::class, result::class)
     }
 }
